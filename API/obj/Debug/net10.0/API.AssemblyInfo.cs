27c9d44338ef30a11def2aa446245c751e259b2c
Robert Pavlik <rpavli5@wgu.edu>
2026-07-13 16:45:20 -0400

    Added Angular Template

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c60b09d49f07d4a4d01c6075b6e67436d9523b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d065dadd52397cf8181890ff3d443dddb8daff6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
