2ca0f71f91325811e8b9cdb39c6115f2c364b1d0
Robert Pavlik <rpavli5@wgu.edu>
2026-07-13 16:17:18 -0400

    Add Git Ignore File

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4eb26a29b34512658d24346facc42bbc330e50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c60b09d49f07d4a4d01c6075b6e67436d9523b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
