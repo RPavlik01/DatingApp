a770d6b53dc79cb4a4f9e0a697b71b0bd3a9c1ae
Robert Pavlik <rpavli5@wgu.edu>
2026-07-13 17:03:21 -0400

    Changes to Angular App Class

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = 'client';
+export class App implements OnInit {
+  
+  private http = inject(HttpClient);
+  protected readonly title = 'Dating App';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost/5001/api/members').subscribe({
+      next: response => console.log(),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    })
+  }
+
 }
